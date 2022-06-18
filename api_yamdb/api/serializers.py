427afc8840db87746d5870f753d4e3ea15eb5915
Семён Капустников <s.v.kapustnikov@gmail.com>
2022-06-18 13:58:55 +0300

    добавил тестовые сериализаторы для User

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-
-from reviews.models import Comment, Review
+from django.contrib.auth.base_user import BaseUserManager
+from reviews.models import Comment, Review, User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -21,3 +22,42 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', ]
+
+    def validate_username(self, username):
+        if username.lower() == 'me':
+            raise serializers.ValidationError(
+                'Нельзя использовать имя "me"')
+        return username
+
+    def create(self, validated_data):
+        password = BaseUserManager().make_random_password()
+        user = User(email=validated_data['email'],
+                    username=validated_data['username'],
+                    password=password,
+                    confirmation_code=password)
+        user.save()
+        return user
+
+
+class TokenSerializer(TokenObtainPairSerializer):
+    username = serializers.CharField(max_length=255)
+    confirmation_code = serializers.CharField(max_length=128)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', ]
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['password'].required = False
+
+        def validate(self, attrs):
+            confirmation_code = attrs.get()['confirmation_code']
+            attrs.update({'password': f'{confirmation_code}'})
+            return super(TokenSerializer, self).validate(attrs)
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,10 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import CommentViewSet, ReviewViewSet
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 app_name = 'api'
 
 v1_router = DefaultRouter()
@@ -21,4 +24,6 @@ v1_router.register(
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

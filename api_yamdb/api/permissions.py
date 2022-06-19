f7cb1b9539d4b86615e626d576f1f15f2a1a9c08
Anastasiya Leontieva <leontieva.anastasiya2015@yandex.ru>
2022-06-19 21:51:24 +0300

    Add permissions (title, cat, gen)

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,41 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class IsAuthorAdminModeratorOrReadOnly(BasePermission):
+    """
+    Позволяет редактировать и удалять объекты только
+    их автору/администратору/модератору.
+    """
+
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            obj.author == request.user
+            or request.user.role in ('admin', 'moderator')
+        )
+
+
+class ReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+
+class IsAdminOrReadOnly(BasePermission):
+    """
+    Позволяет редактировать и удалять объекты только
+    администратору.
+    """
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+           request.user.role in ('admin')
+        )

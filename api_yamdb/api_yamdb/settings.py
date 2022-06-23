8939526cd6961ecb5aea92a681cc360b7419a504
Карина Солнцева <kar-zak1996@ya.ru>
2022-06-23 11:49:29 +0300

    импорт модуля

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'django_filters',
     'reviews',
